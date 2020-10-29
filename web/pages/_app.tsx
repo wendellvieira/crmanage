144ed0d2af3517c4173c7070649fc04ed899d4a7
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-10-29 14:30:39 -0300

    install styled-components

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
-import { AppProps } from 'next/app'
-
 import 'antd/dist/antd.css'
-import '../styles/vars.css'
-import '../styles/global.css'
+
+import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyle from 'styles/global'
+import theme from 'styles/theme'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+            <GlobalStyle />
+        </ThemeProvider>
+    )
 }
