e1a1670113d3b784973b05a561d1e95df1da5f23
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-11-04 16:21:21 -0300

    Clean system

--- backend/src/customer/customer.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CustomerService } from './customer.service';
-import { CustomerResolver } from './customer.resolver';
-
-@Module({
-    providers: [CustomerResolver, CustomerService]
-})
-export class CustomerModule {}
--- backend/src/customer/dto/update-customer.input.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCustomerInput } from './create-customer.input';
-
-export class UpdateCustomerInput extends PartialType(CreateCustomerInput) {
-  id: number;
-}
