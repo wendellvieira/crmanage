140e6a6abeed426224119c9cc1bd4abecebd7d7c
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-11-05 14:56:44 -0300

    Revert "move project"
    This reverts commit 8a9f542a59532a95d731ca72e5532317d912ca23.

---
+++ web/pages/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import InternalSistem from '../layouts/InternalSistem'
+
+export default function Home() {
+    return <InternalSistem title="Clientes">teste1</InternalSistem>
+}
